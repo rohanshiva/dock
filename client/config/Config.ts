65d6738b8f78e2b4aed25c0802d3b793721e0b38
Rohan <rhnshiva@gmail.com>
2022-08-17 18:03:31 -0500

    mv to https

--- client/config/Config.ts
+++ client/config/Config.ts
@@ -1,5 +1,5 @@
 const config = {
-    base: (typeof window !== "undefined") ? `http://${window.location.hostname}:8000/api` : `http://localhost:8000/api`,
+    base: (typeof window !== "undefined") ? `https://${window.location.hostname}:8000/api` : `http://localhost:8000/api`,
     add: "add",
     fetch: "fetch",
     download: "download",
