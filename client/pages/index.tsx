36cedf548d362d9fbb19df33f978688e748015f8
Rohan <rhnshiva@gmail.com>
2022-08-17 20:53:12 -0500

    update app id to use dock app

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,37 +1,43 @@
 import type { NextPage } from 'next'
 import { useRouter } from 'next/router';
-import {Rss} from "react-feather"
+import { useEffect, useState } from 'react';
+import { Rss } from "react-feather"
 import Dock from "../components/dock";
-import {getColor, generateColors, generateRadialGradients} from "../services/Color";
+import { getColor, generateColors, generateRadialGradients } from "../services/Color";
 
 const Home: NextPage = () => {
+
   const router = useRouter();
-  
-  const host = typeof window !== 'undefined' && window.location.host ? window.location.host : '';
+  const [host, setHost] = useState<string>();
+  useEffect(() => {
+    if (router.isReady) {
+      setHost(window.location.host)
+    }
+  }, [router])
 
   // base color
-  const color = getColor(); 
-  
+  const color = getColor();
+
   // get the color for the radial gradient
-  const colors = generateColors(6, color); 
-  
+  const colors = generateColors(6, color);
+
   // generate the radial gradient
-  const proprieties = generateRadialGradients(6, colors); 
+  const proprieties = generateRadialGradients(6, colors);
   return (
     <main>
-    <div className="nav">
-      <div className="pfp" style={{"backgroundImage": proprieties.join(","), "backgroundColor": colors[0]}}>
+      <div className="nav">
+        <div className="pfp" style={{ "backgroundImage": proprieties.join(","), "backgroundColor": colors[0] }}>
+        </div>
+        <h1>
+          sponge
+        </h1>
+        <a href={`https://feeds_${host}`}>
+          <Rss size={16} />
+        </a>
       </div>
-      <h1>
-        sponge
-      </h1>
-      <a href={`https://feeds_${host}`}>
-        <Rss size={16} />
-      </a>
-    </div>
-    <Dock />
-    {/* <Page post={{content: "# hello \nthis is nice \n```def python(:'dhd'):```", is_file: false, key: "167328949327"} as IPost}/> */}
-  </main>
+      <Dock />
+      {/* <Page post={{content: "# hello \nthis is nice \n```def python(:'dhd'):```", is_file: false, key: "167328949327"} as IPost}/> */}
+    </main>
   )
 }
 
